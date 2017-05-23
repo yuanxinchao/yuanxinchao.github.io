7f4d6f989d937cff0e41e8f71c7910333e92c097
yuanxinchao <763455251@qq.com>
2017-05-23 23:23:30 +0800

    video fadeout

---
+++ technology/unity/工具总结/音量fadeout思路/思路.cs
@@ -0,0 +1,12 @@
+﻿       if (soundvolumeScale > 0)
+        {
+            sourceInfo.m_source.Play();
+            float startTime = Time.unscaledTime;
+            float durationTime;
+            while ((durationTime = Time.unscaledTime - startTime) < fadeDuration)
+            {
+                float volumeScale = Mathf.Lerp(0, 1, durationTime / fadeDuration);
+                sourceInfo.m_source.volume = sourceInfo.m_volume * soundvolumeScale * volumeScale;
+                yield return null;
+            }
+        }
