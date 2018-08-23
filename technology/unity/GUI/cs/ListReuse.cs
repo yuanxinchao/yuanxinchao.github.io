acb05b733267e88b6f6ca0b0e9d2121122f4d9bc
Yuanxinchao <763455251@qq.com>
2018-08-23 13:28:48 +0800

    add dic

--- technology/unity/GUI/cs/ListReuse.cs
+++ technology/unity/GUI/cs/ListReuse.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ListReuse<TItem> where TItem:ListItem,new () 
+public class ListReuse<TItem> where TItem : ListItem, new()
 {
     private RectTransform _content;
     private ScrollRect _scroll;
     private float _pfbHight;
     private float _space = 10;
-    private ListView _list; 
+    private ListView _list;
     private int _prefabNum;
 
     private int _infoIndex;
 
-    private Action<ListItem, int> _refresh; 
-    public ListReuse(ScrollRect scroll, RectTransform content, GameObject model,Action<ListItem, int> refreshAc)
+    private Action<ListItem, int> _refresh;
+    public ListReuse(ScrollRect scroll, RectTransform content, GameObject model, Action<ListItem, int> refreshAc)
     {
         _refresh = refreshAc;
         _scroll = scroll;
@@ -23,46 +23,94 @@ public class ListReuse<TItem> where TItem:ListItem,new ()
         var modelRect = model.GetComponent<RectTransform>();
         _pfbHight = modelRect.rect.height;
 
-        _list = ListView.Create(_scroll, content, model);
+        _list = ListView.Create<TItem>(_scroll, content, model);
 
         var viewport = content.parent.GetComponent<RectTransform>();
         _prefabNum = (int)(viewport.rect.height / (_pfbHight + _space)) + 3;//viewport所展示的pfb数量+3
         _scroll.onValueChanged.AddListener(ScrollValueChange);
 
 
-        if(Math.Abs(content.anchorMax.y - 1) > 0.1f || Math.Abs(content.anchorMin.y - 1) > 0.1f)
+        if (Math.Abs(content.anchorMax.y - 1) > 0.1f || Math.Abs(content.anchorMin.y - 1) > 0.1f)
             throw new Exception("content.anchor.min & max .y should =1");
         if (Math.Abs(content.pivot.y - 1) > 0.1f)
             throw new Exception("content.pivot.y should =1");
-        if(Math.Abs(modelRect.pivot.y - 1) > 0.1f)
+        if (Math.Abs(modelRect.pivot.y - 1) > 0.1f)
             throw new Exception("itempfb.pivot.y should =1");
 
     }
 
     private int _infoCount;
 
-    public void SetInfoCount(int infoCount)
+    public void SetBeginAndRefresh(int infoCount)
+    {
+        _scroll.verticalNormalizedPosition = 1;
+        _infoCount = infoCount;
+        SetContentSize();
+        SetIndexListInfo(0);
+    }
+    //info count 所有数据数量
+    public void Refresh(int infoCount)
     {
         _infoCount = infoCount;
+        SetContentSize();
+        int index = NowIndex();
 
-        _content.sizeDelta = new Vector2(_content.sizeDelta.x, (_pfbHight + _space) * _infoCount - _space);
-        _content.anchoredPosition = new Vector2(_content.anchoredPosition.x,0);
-
-        int count = _prefabNum < _infoCount ? _prefabNum : _infoCount;
-        
-        _list.Clear();
-        for (int i = 0; i < count; i++)
-        {
-            ListItem item = _list.AddItem<TItem>();
-            _refresh(item, i);
-            item.transform.localPosition = new Vector3(item.transform.localPosition.x,
-                CalculateYByIndex(i),
-                item.transform.localPosition.z);
-        }
-        _infoIndex = 0;
+        SetIndexListInfo(index);
 
     }
 
+    private void SetIndexListInfo(int index)
+    {
+        int count = _infoCount < _prefabNum ? _infoCount : _prefabNum;
+        _list.Clear();
+        int num = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (i + index < _infoCount)
+            {
+                SetIndexInfo(i + index, true);
+                num++;
+            }
+            else
+                break;
+
+        }
+        for (; num < count; num++)
+        {
+            if (index - 1 >= 0)
+            {
+                SetIndexInfo(index - 1, false);
+                index--;
+            }
+            else
+                break;
+        }
+        _infoIndex = index;
+
+    }
+
+    private void SetIndexInfo(int index, bool endAdd)
+    {
+        ListItem item;
+        if (endAdd)
+            item = _list.AddItem<TItem>();
+        else
+            item = _list.InsertItem<TItem>(0);
+        _refresh(item, index);
+        item.transform.localPosition = new Vector3(item.transform.localPosition.x,
+            CalculateYByIndex(index),
+            item.transform.localPosition.z);
+    }
+    private void SetContentSize()
+    {
+        _content.sizeDelta = new Vector2(_content.sizeDelta.x, GetLength(_infoCount));
+    }
+
+    //pfb count -> content length
+    private float GetLength(int count)
+    {
+        return (_pfbHight + _space) * count - _space;
+    }
     private float CalculateYByIndex(int index)
     {
         return -index * (_space + _pfbHight);
@@ -70,14 +118,14 @@ public class ListReuse<TItem> where TItem:ListItem,new ()
 
     private void ScrollValueChange(Vector2 arg0)
     {
-        int index =(int)(_content.anchoredPosition.y/(_space + _pfbHight));
+        int index = NowIndex();
 
         while (index - 1 > _infoIndex && _infoIndex + _prefabNum < _infoCount)
         {
             int end = _infoIndex + _prefabNum;
             _list.RemoveBegin();
             ListItem item = _list.AddItem<TItem>();
-            _refresh(item,end);
+            _refresh(item, end);
             item.transform.localPosition = new Vector3(item.transform.localPosition.x,
                                             CalculateYByIndex(end),
                                             item.transform.localPosition.z);
@@ -87,7 +135,7 @@ public class ListReuse<TItem> where TItem:ListItem,new ()
         while (index - 1 < _infoIndex && _infoIndex - 1 >= 0)
         {
             _list.RemoveEnd();
-            ListItem item = _list.Insert<TItem>(0);
+            ListItem item = _list.InsertItem<TItem>(0);
             _refresh(item, _infoIndex - 1);
             item.transform.localPosition = new Vector3(item.transform.localPosition.x,
                                             CalculateYByIndex(_infoIndex - 1),
@@ -96,13 +144,20 @@ public class ListReuse<TItem> where TItem:ListItem,new ()
             _infoIndex--;
         }
     }
+
+    private int NowIndex()
+    {
+        int index = (int)(_content.anchoredPosition.y / (_space + _pfbHight));
+
+        return Mathf.Clamp(index, 0, _infoCount - 1);
+    }
     /// <summary>
     /// refreshAc 参数为刷新info某个index的action
     /// </summary>
     /// <param name="listRootTransform"></param>
     /// <param name="refreshAc"></param>
     /// <returns></returns>
-    public static ListReuse<TItem> Create(Transform listRootTransform,Action<ListItem,int> refreshAc)
+    public static ListReuse<TItem> Create(Transform listRootTransform, Action<ListItem, int> refreshAc)
     {
 
         var scroll = listRootTransform.GetComponent<ScrollRect>();
@@ -122,9 +177,9 @@ public class ListReuse<TItem> where TItem:ListItem,new ()
         if (!itemModel)
             throw new Exception("list item model is null! root gameobject: " + listRootTransform.gameObject.name);
 
-        if(content.GetComponent<LayoutGroup>()!=null)
+        if (content.GetComponent<LayoutGroup>() != null)
             throw new Exception("content needn't LayoutGroup");
-        if(content.GetComponent<ContentSizeFitter>()!=null)
+        if (content.GetComponent<ContentSizeFitter>() != null)
             throw new Exception("content needn't ContentSizeFitter");
 
         ListReuse<TItem> list = new ListReuse<TItem>(scroll, content, itemModel, refreshAc);
