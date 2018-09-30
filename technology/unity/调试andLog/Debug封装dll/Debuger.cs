0142a416f09d4d20a05b8be4a8bd05f2b3b1bd22
Yuanxinchao <763455251@qq.com>
2018-09-30 16:56:59 +0800

    add debug change directory hierarchy

---
+++ technology/unity/调试andLog/Debug封装dll/Debuger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+public class Debuger
+{
+    private static bool enableLog = true;
+
+    public static bool EnableLog
+    {
+        get
+        {
+            return enableLog;
+        }
+
+        set
+        {
+            enableLog = value;
+        }
+    }
+
+    static public void Log(object message)
+    {
+        Log(message,null);
+    }
+    static public void Log(object message, Object context)
+    {
+        if (EnableLog)
+        {
+            Debug.Log("YXC  "+message, context);
+        }
+    }
+}
