575200f183b5ee7b8f667a811e6970d17adc0c05
Yuanxinchao <763455251@qq.com>
2019-01-02 09:47:25 +0800

    add dbCheck and TabBtns TabViews

---
+++ technology/python/CheckDB/dbCheck.py
@@ -0,0 +1,86 @@
+﻿# -*- coding:utf-8 -*-
+'''
+Created on 2017-12-25
+
+@author: rex
+'''
+import sqlite3
+import os
+import re
+
+DB_FILE = "p16.db"
+
+
+def sqlTypeToCSharpType(sqlType):
+    if sqlType == "int":
+        return "int"
+    elif sqlType in ["varchar", "text"]:
+        return "string"
+    elif sqlType in ["float"]:
+        return "float"
+    elif sqlType in ["bigint"]:
+        return "long"
+    else:
+        return sqlType
+
+
+# 匹配 , | 格式 的字串
+Format = r'^(/d+\|/d+,)*/d+\|/d+$'
+# cfg_ship_display showShip
+showShip = r'^([0-9]{8}\|[01],)*[0-9]{8}\|[01]$'
+# cfg_ship_shop_unlock unit_pos
+unitPos = r'^([0-9]{8}\|[01],)*[0-9]{8}\|[01]$'
+
+
+# 验证cfg_ship_display 格式
+def CheckShowShip(cu_sqlite):
+    cu_sqlite.execute("select id,show_ship from cfg_ship_display")
+    showShips = cu_sqlite.fetchall()
+    for row in showShips:
+        matchObj = re.match(showShip, row[1])
+        if matchObj:
+            pass
+        else:
+            print "cfg_ship_display->show_ship doesn't match rule" + " id=" + str(row[0])
+
+        result = row[1].split(',')
+        ships = set()
+        for s in result:
+            ship = s.split('|')
+            if ship[0] in ships:
+                print "cfg_ship_display->show_ship repeat shipid=" + ship[0] + " id=" + str(row[0])
+            else:
+                ships.add(ship[0])
+
+# 验证cfg_ship_shop_unlock 格式
+def CheckShipShopUnlock(cu_sqlite):
+    cu_sqlite.execute("select ship_id,unit_pos from cfg_ship_shop_unlock")
+    results = cu_sqlite.fetchall()
+    for row in results:
+        matchObj = re.match(r'^([0-6](\|-*[0-9]+){4},)*[0-6](\|-*[0-9]+){4}$', row[1])
+        if matchObj:
+            pass
+        else:
+            print "cfg_ship_shop_unlock->unit_pos doesn't match rule" + " shipid=" + str(row[0])
+
+def CheckDB():
+    global DB_NAME, DB_FILE
+    cx_sqlite = sqlite3.connect(DB_FILE)
+    cu_sqlite = cx_sqlite.cursor()
+    CheckShowShip(cu_sqlite)
+    CheckShipShopUnlock(cu_sqlite)
+    cx_sqlite.commit()
+    cu_sqlite.close()
+    cx_sqlite.close()
+    print "Check finish"
+
+
+if __name__ == '__main__':
+    import sys
+
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    if os.path.exists(DB_FILE):
+        CheckDB()
+    else:
+        print ("ERROR")
---
+++ technology/unity/GUI/cs/TabsHelper.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+public class TabHelper
+{
+    public List<ITab> _tabs = new List<ITab>(4);
+    private int _currentSpId = -1;
+    public Action<int, int> OnTabSwitch { get; set; }
+
+    public void Finish()
+    {
+        var tab = GetTabBySpId(_currentSpId, true);
+        if (tab == null)
+        {
+            if (_tabs.Count > 0)
+                Click(_tabs[0]);
+        }
+        else
+        {
+            Click(tab);
+        }
+    }
+    public void Clear()
+    {
+        for (int i = _tabs.Count - 1; i >= 0; i--)
+        {
+            RemoveIndex(i);
+        }
+    }
+    public void ClearCurrentSpId()
+    {
+        _currentSpId = -1;
+    }
+    public void ClickBySpId(int spId)
+    {
+        if (_currentSpId != -1 && _currentSpId != spId)
+        {
+            var lastTab = GetTabBySpId(_currentSpId, true);
+            if (lastTab != null)
+            {
+                lastTab.Unselect();
+            }
+        }
+
+        if (_currentSpId != spId)
+        {
+            ITab tab = GetTabBySpId(spId);
+            tab.Select();
+        }
+
+        if (OnTabSwitch != null)
+            OnTabSwitch(_currentSpId, spId);
+
+        _currentSpId = spId;
+    }
+    public void Click(ITab tab)
+    {
+        ClickBySpId(tab.SpId);
+    }
+    public void ClickByIndex(int index)
+    {
+        Click(_tabs[index]);
+    }
+    private void AddTab(ITab tab)
+    {
+        Insert(_tabs.Count, tab);
+    }
+    public void AddTab(ITab tab,int spId)
+    {
+        tab.SpId = spId;
+        AddTab(tab);
+    }
+    public void Insert(int index, ITab tab)
+    {
+        if (index > _tabs.Count)
+            throw new Exception("can't insert to list, index=" + index + " count=" + _tabs.Count);
+
+        _tabs.Insert(index, tab);
+
+        for (int i = index; i < _tabs.Count; i++)
+        {
+            _tabs[i].Index = i;
+        }
+
+        if (tab.SpId == _currentSpId)
+        {
+            tab.Select();
+        }
+        else
+        {
+            tab.Unselect();
+        }
+    }
+    public void RemoveSpId(int spId)
+    {
+        ITab tab = GetTabBySpId(spId);
+        Remove(tab);
+    }
+    public void Remove(ITab tab)
+    {
+        int index = _tabs.IndexOf(tab);
+        if (index == -1)
+            throw new Exception("can't remove tab SpId=" + tab.SpId);
+
+        RemoveIndex(index);
+    }
+
+    public void RemoveIndex(int index)
+    {
+        _tabs.RemoveAt(index);
+        for (int i = index; i < _tabs.Count; i++)
+        {
+            _tabs[i].Index = i;
+        }
+    }
+
+    public ITab GetTabBySpId(int spId, bool canBeNull = false)
+    {
+        for (int i = 0; i < _tabs.Count; i++)
+        {
+            if (_tabs[i].SpId == spId)
+                return _tabs[i];
+        }
+        if (canBeNull)
+            return null;
+
+        throw new Exception("can't find tabBtn spId=" + spId);
+    }
+
+    public List<ITab> GetTabList()
+    {
+        return _tabs;
+    }
+
+    public void Close()
+    {
+        var tab = GetTabBySpId(_currentSpId, true);
+        if (tab != null)
+            tab.Unselect();
+    }
+}
+public class TabBtnHelper
+{
+    private TabHelper _tabHelper;
+    private TabBtnPool _createTab;
+
+    public Action<int, int> OnTabSwitch {get { return _tabHelper.OnTabSwitch; }set { _tabHelper.OnTabSwitch = value; }
+    }
+    public TabBtnHelper(RectTransform root, GameObject model)
+    {
+        _tabHelper = new TabHelper();
+        _createTab = new TabBtnPool(root, model);
+    }
+
+    public T AddTab<T>(int spId) where T : Behaviour, ITab
+    {
+        T tab = _createTab.Get<T>(spId);
+        _tabHelper.AddTab(tab, spId);
+        tab.transform.gameObject.SetActive(true);
+        tab.transform.GetComponent<Button>().onClick.SetListener(() => _tabHelper.Click(tab));
+        return tab;
+    }
+
+    public void Clear()
+    {
+        var tabs = _tabHelper.GetTabList();
+        for (int i = tabs.Count - 1; i >= 0; i--)
+        {
+            RemoveIndex(i);
+        }
+    }
+
+    public void RemoveIndex(int index)
+    {
+        var tab = _tabHelper.GetTabList()[index];
+        _createTab.Put(tab);
+        _tabHelper.RemoveIndex(index);
+    }
+    public void Finish()
+    {
+        _tabHelper.Finish();
+    }
+    public List<ITab> GetTabList()
+    {
+        return _tabHelper.GetTabList();
+    }
+}
+public abstract class TabViewBase2 : Behaviour, ITab
+{
+    public int SpId { get; set; }
+    public int Index { get; set; }
+    public virtual void Select()
+    {
+        transform.gameObject.SetActive(true);
+    }
+
+    public virtual void Unselect()
+    {
+        transform.gameObject.SetActive(false);
+    }
+}
+
+public interface ITab
+{
+    int SpId { get; set; }
+    int Index { get; set; }
+
+    void Select();
+
+    void Unselect();
+
+}
+public class TabBtnPool
+{
+    private GameObject _model;
+    private RectTransform _root;
+
+    private Stack<ITab> _tabFreeList = new Stack<ITab>();
+
+    public TabBtnPool(RectTransform root, GameObject model)
+    {
+        _root = root;
+        _model = model;
+        _model.gameObject.SetActive(false);
+    }
+    public T Create<T>() where T : Behaviour, ITab
+    {
+        var tab = Object.Instantiate(_model, _root);
+        T t = tab.AddBehaviour<T>();
+        return t;
+    }
+    public void Put(ITab tab) 
+    {
+        ((Behaviour)tab).transform.gameObject.SetActive(false);
+        _tabFreeList.Push(tab);
+    }
+
+    public T Get<T>(int spId) where T : Behaviour, ITab
+    {
+        T ret = null;
+        int count = _tabFreeList.Count;
+        if (count > 0)
+        {
+            ret = (T)_tabFreeList.Pop();
+        }
+        if (ret == null)
+        {
+            ret = Create<T>();
+        }
+        return ret;
+    }
+}
